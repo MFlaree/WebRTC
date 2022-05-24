a2b237963088b8dcad891d4f29fbaa3ec4b1e3fa
@sc4llejas <sc4llejas@gmail.com>
2022-05-24 20:14:03 +0200

    m1

--- client/src/comps/chat/elements/Mensajes.js
+++ client/src/comps/chat/elements/Mensajes.js
@@ -1,16 +1,24 @@
-import { io } from 'socket.io-client';
+import { useState, useEffect } from "react";
+
+import { io } from "socket.io-client";
 const WEB = "http://localhost:3002";
 const socket = io(WEB);
 
 function Mensajes() {
 
+  const [msg, setMsg] = useState([]);
+
   return (
     <section className="Mensajes">
-        <p><label htmlFor="user">User: </label><input id="user" type="text" /></p>
-        <div>
-
-        </div>
-        <p><input id="msg" type="text" placeholder="Enter message" /><button>Send</button></p>
+      <p>
+        <label htmlFor="user">User: </label>
+        <input id="user" type="text" />
+      </p>
+      <div></div>
+      <p>
+        <input id="msg" type="text" placeholder="Enter message" />
+        <button>Send</button>
+      </p>
     </section>
   );
 }
--- client/src/comps/videochat/elements/Video.js
+++ client/src/comps/videochat/elements/Video.js
@@ -1,51 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 const WEB = "http://localhost:3002";
 const socket = io(WEB);
 
 function Video() {
-
   const [streamState, setStreamState] = useState(false);
-  let error = useState(false);
 
-  async function toggleVideo() {   
-    try {
-
+  useEffect(() => {
+    async function callback() {
       const video = document.getElementById("video");
       const canvas = document.getElementById("canvas");
-
+      let interval;
       if (streamState) {
-        video.srcObject.getTracks()[0].stop();
-
-      } else {
         video.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
-
-        setInterval(() => {
+        // !Cambiar dibujar por enviar imagen al servidor
+        interval = setInterval(() => {
           canvas.width = video.videoWidth;
           canvas.height = video.videoHeight;
           canvas.getContext("2d").drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+          socket.emit("videochat","AYAYA");
         }, 40);
-        
+      } else {
+        if (video.srcObject !== null) {     //Condición para que no salte un error en la inicialización.
+          video.srcObject.getTracks()[0].stop();
+          clearInterval(interval);    // !Que esta función funcione.
+        }
       }
-      
-      setStreamState(!streamState);
-
-    } catch (err) {
-      error = err;
-      console.log(error);
     }
-    
-  }
+    try {
+      callback();
+    } catch (err) {
+      console.log(err);
+    }
+  }, [streamState]);
 
   return (
-    <section className='VideoChat'>
-      <button onClick={toggleVideo}>Start Broadcasting</button>
-      <br/>
+    <section className="VideoChat">
+      <button onClick={() => setStreamState(!streamState)}>Start Broadcasting</button>
+      <br />
       <video id="video" autoPlay></video>
       <canvas id="canvas"></canvas>
     </section>
-    
   );
 }
 
